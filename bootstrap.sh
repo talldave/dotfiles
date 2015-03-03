666433d93d2fa342d1c8fc87d7ecff1d9a6b0653
Dave Bianco <dbianco@urthecast.com>
2015-03-02 16:14:44 -0800

    modified:   .bash_profile 	deleted:    .git_repo 	modified:   bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,34 +2,54 @@
 # bootstrap.sh
 
 dir=~/.dotfiles
-bash_files=".bash_logout .bash_profile .bash_colors .bash_prompt" 
-git_files=".git_completion .gitignore_global .git_prompt .gitconfig"
-vim_files=".vim .vimrc"
+bash_files=".bash_logout .bash_profile .bash_colors .bash_prompt " 
+git_files=".git_completion .gitignore_global .git_prompt .gitconfig "
+vim_files=".vim .vimrc "
 files=""
+git=""
 
-case "$1" in
-    b)
-        files+=$bash_files
-        ;;
-    g)
-        files+=$git_files
-        # Enter git username
-        echo "Enter your Git username: "
-        read gitu
-        git config --global user.name "${gitu}"
-        # Enter git email
-        echo "Enter your Git email: "
-        read gite
-        git config --global user.email "${gite}"
-        ;;
-    v)
-        files+=$vim_files
-        ;;
-    *)
-        echo "Use -gbv to instantiate git bash vim dotfiles"
-        exit 1
-        ;;
-esac
+function usage {
+    echo "USAGE: $0"
+    echo
+    echo "-b : include .bash*"
+    echo "-g : include .git*"
+    echo "-v : include .vim*"
+    exit
+}
+
+function git_config {
+    git config --global user.name "${gitu}"
+    git config --global user.email "${gite}"
+}
+
+
+if [ "$1" = "" ]
+then
+    usage
+fi
+
+while getopts gbv flag
+do
+    case ${flag} in
+        b)
+            files+=$bash_files
+            ;;
+        g)
+            files+=$git_files
+            echo -n "Enter your Git username: "
+            read gitu
+            echo -n "Enter your Git email: "
+            read gite
+            git=1
+            ;;
+        v)
+            files+=$vim_files
+            ;;
+        ?)
+            usage
+            ;;
+    esac
+done
 
 for file in $files
 do
@@ -38,4 +58,8 @@ do
     ln -s $dir/$file ~/$file
 done
 
+if [ "${git}" -eq 1 ]
+then
+    git_config
+fi
 
